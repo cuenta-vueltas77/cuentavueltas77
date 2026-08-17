a1976a8cbd5b0f28622c064ad9219d2194e841f8
cuenta-vueltas77 <taculantonella@gmail.com>
2026-08-17 15:08:46 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,8 @@
 import { defineConfig } from 'astro/config';
-import db from '@astrojs/db'; // <-- 1. Tiene que estar este import
-
-import node from '@astrojs/node';
+import db from '@astrojs/db';
 
 export default defineConfig({
   integrations: [
-    db() // <-- 2. Tiene que estar agregado acá adentro
-  ],
-
-  adapter: node({
-    mode: 'standalone',
-  }),
+    db()
+  ]
 });
