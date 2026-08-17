e1f79fba3c39c16620283b44cbb37f1b681b958f
cuenta-vueltas77 <taculantonella@gmail.com>
2026-08-17 15:43:23 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
 import db from '@astrojs/db';
-import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  output: 'server',
   integrations: [
     db()
-  ],
-  adapter: vercel(),
+  ]
 });
